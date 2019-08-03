29d62767998351866cf18805d7411f6ec63dd909
Aleksandr <89035960218@mail.ru>
2019-08-03 16:09:56 +0300

    lesson2

---
+++ lesson2/src/lesson2/MyArrayDataException.java
@@ -0,0 +1,11 @@
+package lesson2;
+
+public class MyArrayDataException extends Exception{
+    public int row;
+    public int column;
+
+    public MyArrayDataException(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+}
