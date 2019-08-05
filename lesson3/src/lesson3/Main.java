1d262d084adf9887bf8601075d0d345488e54ea4
Aleksandr <89035960218@mail.ru>
2019-08-05 15:29:31 +0300

    lesson3

---
+++ lesson3/src/lesson3/Main.java
@@ -0,0 +1,14 @@
+package lesson3;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayOfWords aw = new ArrayOfWords();
+        aw.arrayOfWords();
+//  Второй пункт задания не успел доделать. Чуть позже пришлю.
+//        TelephoneDirectory td = new TelephoneDirectory();
+//        td.telephoneDirectory();
+    }
+}
